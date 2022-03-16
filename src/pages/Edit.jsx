a3d41e49dd6cdceda679f3cca8c592269f0c96a9
GilHwan <dyghks123@gmail.com>
2022-03-16 17:55:44 +0900

    ✨ React Params, Navigate using

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 const Edit = () => {
-  return <div> EDIT</div>;
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get('id'); // get 쿼리 스트링 사용
+  console.log('id는:', id);
+
+  const mode = searchParams.get('mode');
+  console.log('mode는', mode);
+
+  return (
+    <div>
+      <h2>EDIT</h2>
+      <button onClick={() => setSearchParams({ who: 'hwan' })}>
+        qs 바꾸기
+      </button>
+
+      <button onClick={() => navigate('/home')}> home으로 ㄱㅂㄹㄱ</button>
+    </div>
+  );
 };
 
 export default Edit;
