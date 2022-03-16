b8a49ab9e272faf46b4faaf3c0fd9780fcb4830c
GilHwan <dyghks123@gmail.com>
2022-03-16 17:30:32 +0900

    ✨  React Router install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+
+import RouteTest from './components/RouteTest';
 
 function App() {
   return (
-    <div className="App">
-      <h2>GGGG</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>GGGG</h2>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
