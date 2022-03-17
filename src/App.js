975b4e5ecbb146a2abbad03a9dfd565c2f97d0df
GilHwan <dyghks123@gmail.com>
2022-03-17 21:53:26 +0900

    💄  Implement Header component style

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h2>GGGG</h2>
+        <MyHeader
+          headeText={'APP'}
+          leftChild={
+            <MyButton text={'왼쪽'} onClick={() => alert('뒤로가기')} />
+          }
+          rightChild={
+            <MyButton text={'오른쪽'} onClick={() => alert('앞으로가기')} />
+          }
+        />
+
+        <h2>App.js</h2>
 
         <MyButton
           text={'버튼'}
---
+++ src/components/MyHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MyHeader = ({ headeText, leftChild, rightChild }) => {
+  return (
+    <header>
+      <div className="head_btn_left">{leftChild}</div>
+      <div className="head_text">{headeText}</div>
+      <div className="head_btn_right">{rightChild}</div>
+    </header>
+  );
+};
+
+export default MyHeader;
