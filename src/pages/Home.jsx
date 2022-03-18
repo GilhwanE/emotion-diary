975e9ab13a0fa84ae6b9d4e1d189df00f6c9be7d
GilHwan <dyghks123@gmail.com>
2022-03-18 17:24:31 +0900

    ✨  Create home component button, date

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MyButton from '../components/MyButton';
+import MyHeader from './../components/MyHeader';
 
 const Home = () => {
-  return <div> Home</div>;
+  const [curDate, setCurDate] = useState(new Date());
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월 `;
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
+  return (
+    <div>
+      <MyHeader
+        headeText={headText}
+        leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
+      />
+    </div>
+  );
 };
 
 export default Home;
