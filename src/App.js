291a87536a2fc4f7210a7407abfcf4cf79745429
GilHwan <dyghks123@gmail.com>
2022-03-20 17:57:46 +0900

    🐛  Unnecessary content annotation

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-import MyButton from './components/MyButton';
-import MyHeader from './components/MyHeader';
 import React, { useReducer, useRef } from 'react';
 
 const reducer = (state, action) => {
@@ -116,15 +114,13 @@ function App() {
       <DiaryDispatchContext.Provider value={{ onCreate, onRemove, onEdit }}>
         <BrowserRouter>
           <div className="App">
-            <MyHeader />
-            <h2>App.js</h2>
-            <MyButton
+            {/* <MyButton
               text={'버튼'}
               onClick={() => alert('버튼 클릭')}
               type={'positive'}
             />
             <MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} />
-            <MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} />
+            <MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} /> */}
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/new" element={<New />} />
--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, useNavigate } from 'react-router';
+import { useNavigate } from 'react-router';
 import MyButton from './MyButton';
 
 const DiaryItem = ({ id, emotion, content, date }) => {
