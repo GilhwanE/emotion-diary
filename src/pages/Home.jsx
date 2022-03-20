514c268b7c9c09ca3bdecbb008baa5b319a8e3b6
GilHwan <dyghks123@gmail.com>
2022-03-20 15:50:29 +0900

    ✨   Add to state ( data,setData) , useEffect ( when date change )

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,37 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { DiaryStateContext } from '../App';
+import DiaryList from '../components/DiaryList';
 import MyButton from '../components/MyButton';
 import MyHeader from './../components/MyHeader';
 
 const Home = () => {
-  const [curDate, setCurDate] = useState(new Date());
+  const diaryList = useContext(DiaryStateContext);
 
+  const [data, setData] = useState([]);
+  const [curDate, setCurDate] = useState(new Date());
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월 `;
+
+  // 날짜가 변경될때 변화하도록
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      // diaryList가 비어있는 상태일때는 실행할필요가 없음
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime(); // 이번년도 이번월 1일
+
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0
+      ).getTime();
+
+      setData(
+        diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, curDate]); //diaryList가 바뀌었을때 ( 생성 삭제 )
 
   const increaseMonth = () => {
     setCurDate(
@@ -26,6 +52,7 @@ const Home = () => {
         leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
         rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
       />
+      <DiaryList diaryList={data} />
     </div>
   );
 };
