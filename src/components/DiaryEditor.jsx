d88500c45bd1b23eb80900e2cc5e89731012be86
GilHwan <dyghks123@gmail.com>
2022-03-23 15:22:34 +0900

    ✏️  Move to emotionList and add to util files

--- src/components/DiaryEditor.jsx
+++ src/components/DiaryEditor.jsx
@@ -4,38 +4,14 @@ import { DiaryDispatchContext } from '../App';
 import EmotionItem from './EmotionItem';
 import MyButton from './MyButton';
 import MyHeader from './MyHeader';
+import { emotionList } from '../util/emotion';
 
 const getStringDate = (date) => {
   return date.toISOString().slice(0, 10);
 };
 
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_desc: '완전 나쁨',
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_desc: '나쁨',
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_desc: '그럭저럭',
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_desc: '좋음',
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_desc: '완전 좋음',
-  },
-];
+const env = process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || '';
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
